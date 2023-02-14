bb7c4fbaf48a62add121f622ff124d90c0229a57
GaneshRvernekar <ganeshsantosh19@gmail.com>
2023-02-14 21:52:36 +0530

    longest biotonic sequence

---
+++ biotonic_sequence.cpp
@@ -0,0 +1,54 @@
+	int LongestBitonicSequence(vector<int>nums)
+	{
+	    // code here
+	    int n= nums.size();
+	    
+	    vector<int> subset;
+	    vector<int> dp(n,0);
+	    vector<int> dp2(n,0);
+	    
+	    subset.push_back(nums[0]);
+	    dp[0]=1;
+	    dp2[n-1]=1;
+	    
+	    for(int i=1;i<n;i++)
+	    {
+	        if(subset.back()<nums[i])
+	        {
+	            subset.push_back(nums[i]);
+	        }
+	        else
+	        {
+	            auto it =lower_bound(subset.begin(),subset.end(),nums[i]);
+	            subset[it-subset.begin()]=nums[i];
+	        }
+	        dp[i]=subset.size();
+	    }
+	    subset.clear();
+	    subset.push_back(nums[n-1]);
+	    
+	    for(int i=n-2;i>=0;i--)
+	    {
+	        if(subset.back()<nums[i])
+	        {
+	            subset.push_back(nums[i]);
+	        }
+	        else
+	        {
+	            auto it =lower_bound(subset.begin(),subset.end(),nums[i]);
+	            subset[it-subset.begin()]=nums[i];
+	        }
+	        dp2[i]=subset.size();
+	    }
+	    
+	    int bio=0;
+	    
+	    for(int i=0;i<dp.size();i++)
+	    {
+	            bio = max(bio,dp[i]+dp2[i]-1);
+	    }
+	    
+	    return bio;
+	    
+	    
+	}
