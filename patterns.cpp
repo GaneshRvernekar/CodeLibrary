ef1728a6b1fd9fbe44d2a5f99040fb9f434949e9
GaneshRvernekar <ganeshsantosh19@gmail.com>
2023-02-26 11:34:14 +0530

    pqtterns

--- patterns.cpp
+++ patterns.cpp
@@ -2,11 +2,11 @@
 
 using namespace std;
 
-int main()
+void stars()
 {
-
     int n;
-    cin>>n;
+
+     cin>>n;
 
     for(int i=1;i<=n;i++)
     {
@@ -28,4 +28,118 @@ int main()
         }
         cout<<endl;
     }
+}
+
+void letters()
+{
+    int n;
+    cin>>n;
+
+    for(int i=0;i<=n;i++)
+    {
+        char a = 'A'+(n-i);
+        for(int j=0;j<=i;j++)
+        {
+            cout<<a++<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void diamond(int n)
+{
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<=(2*n);j++)
+        {
+            if(j<(n-i))
+            {
+                cout<<'*';
+            }
+            else if(j>(n+i))
+            {
+                cout<<'*';
+            }
+            else 
+            {
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+
+    for(int i=0;i<n;i++)
+       {
+        for(int j=0;j<=(2*n);j++)
+        {
+            if(j<=i)
+            {
+                cout<<'*';
+            }
+            else if(j>=(2*n-i))
+            {
+                cout<<'*';
+               
+            }
+            else
+            {
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+       }
+
+}
+
+void damaru(int n)
+{
+    for(int i=0;i<n;i++)
+       {
+        for(int j=0;j<=(2*n);j++)
+        {
+            if(j<=i)
+            {
+                cout<<'*';
+            }
+            else if(j>=(2*n-i))
+            {
+                cout<<'*';
+               
+            }
+            else
+            {
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+       }
+
+       for(int i=1;i<n;i++)
+    {
+        for(int j=0;j<=(2*n);j++)
+        {
+            if(j<(n-i))
+            {
+                cout<<'*';
+            }
+            else if(j>(n+i))
+            {
+                cout<<'*';
+            }
+            else 
+            {
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    damaru(n);
 }
